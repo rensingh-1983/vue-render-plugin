cdf03e83150e0a7a4758b100b7aaba761f1a9f67
“ren” <ren.singh@redcrackle.com>
2019-09-13 18:20:00 +0530

    Adding description for socket and label

--- src/Node.vue
+++ src/Node.vue
@@ -3,8 +3,14 @@
   .title {{node.name}}
 
   // Outputs
-  .output(v-for='output in outputs()' :key="output.key")
-    .output-title {{output.name}}
+  .output(v-for='output in outputs()' :key="output.key" @mouseover="hover = node.name+'_ '+output.name" @mouseleave="hover = null" class="tooltip")
+    .output-title(
+      :class="output.name"
+    ) {{output.name}}
+    <div class="output-tooltip tooltiptext" v-if="hover === node.name+'_ '+output.name">
+      <div class="socket_type">Type: {{output.socket.name}}</div>
+      <div class="socket_description">{{output.description}}</div>
+    </div>
     Socket(v-socket:output="output", type="output", :socket="output.socket")
 
   // Controls
@@ -14,9 +20,15 @@
   )
 
   // Inputs
-  .input(v-for='input in inputs()' :key="input.key")
+  .input(v-for='input in inputs()' :key="input.key" @mouseover="hover = node.name+'_ '+input.name"  @mouseleave="hover = null" class="tooltip")
     Socket(v-socket:input="input", type="input", :socket="input.socket")
-    .input-title(v-show='!input.showControl()') {{input.name}}
+    .input-title(
+      v-show='!input.showControl()', 
+    ) {{input.name}}
+    <div class="input-tooltip tooltiptext" v-if="hover === node.name+'_ '+input.name">
+      <div class="socket_type">Type: {{input.socket.name}}</div>
+      <div class="socket_description">{{input.description}}</div>
+    </div>
     .input-control(
       v-show='input.showControl()'
       v-control="input.control"
@@ -31,6 +43,11 @@ export default {
   mixins: [mixin],
   components: {
     Socket
+  },
+  data() {
+    return {
+      hover: null,
+    };
   }
 }
 </script>
@@ -78,4 +95,21 @@ export default {
     display: inline-block
   .control
     padding: $socket-margin $socket-size/2 + $socket-margin
+.tooltip
+  .tooltiptext
+    width: $node-width
+    background-color: black
+    color: #fff
+    padding: 5px 10px
+    border-radius: 6px
+    position: absolute
+    text-align: left
+    z-index: -1
+    .socket_type
+      font-weight: bold
+      font-size: 14px
+  .output-tooltip
+    left: 105%
+  .input-tooltip
+    right: 105%
 </style>
