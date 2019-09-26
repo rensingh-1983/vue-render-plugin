e224398520417c39f08a9d9a679580eefc9fcf5a
“ren” <ren.singh@redcrackle.com>
2019-09-26 17:27:16 +0530

    adding node file

--- src/Node.vue
+++ src/Node.vue
@@ -1,20 +1,12 @@
 <template lang="pug">
 .node(:class="[selected(), node.name] | kebab")
-  .title(class="tooltip") {{node.name}} <span  v-if="typeof node.description !== 'undefined' && node.description.length > 0 " class="node_info" @mouseover="hover = node.name" @mouseleave="hover = null">?</span>
-    <div class="node-tooltip tooltiptext" v-if="hover === node.name">
-      <div class="node_description">{{node.description}}</div>
-    </div>
+  .title(class="tooltip") {{node.name}} <span  v-if="typeof node.description !== 'undefined' && node.description.length > 0 " class="node_info"  v-tooltip.top="node.description">?</span>
   // Outputs
-  .output(v-for='output in outputs()' :key="output.key" @mouseover="hover = node.name+'_ '+output.name" @mouseleave="hover = null" class="tooltip")
+  .output(v-for='output in outputs()' :key="output.key" @mouseover="hover = node.name+'_ '+output.name" @mouseleave="hover = null" class="tooltip" v-tooltip.right-start="{ content: '<div>Type: '+output.socket.name+ '</div><div>' + output.description + ' </div>'}")
     .output-title(
       :class="output.name"
     ) {{output.name}}
-    <div class="output-tooltip tooltiptext" v-if="hover === node.name+'_ '+output.name">
-      <div class="socket_type">Type: {{output.socket.name}}</div>
-      <div class="socket_description">{{output.description}}</div>
-    </div>
     Socket(v-socket:output="output", type="output", :socket="output.socket")
-
   // Controls
   .control(
     v-for='control in controls()',
@@ -22,15 +14,11 @@
   )
 
   // Inputs
-  .input(v-for='input in inputs()' :key="input.key" @mouseover="hover = node.name+'_ '+input.name"  @mouseleave="hover = null" class="tooltip")
+  .input(v-for='input in inputs()' :key="input.key" @mouseover="hover = node.name+'_ '+input.name"  @mouseleave="hover = null" class="tooltip" v-tooltip.left-start="{ content: '<div>Type: '+input.socket.name+ '</div><div>' + input.description + ' </div>'}")
     Socket(v-socket:input="input", type="input", :socket="input.socket")
     .input-title(
       v-show='!input.showControl()', 
     ) {{input.name}}
-    <div class="input-tooltip tooltiptext" v-if="hover === node.name+'_ '+input.name">
-      <div class="socket_type">Type: {{input.socket.name}}</div>
-      <div class="socket_description">{{input.description}}</div>
-    </div>
     .input-control(
       v-show='input.showControl()'
       v-control="input.control"
@@ -107,26 +95,4 @@ export default {
     display: inline-block
   .control
     padding: $socket-margin $socket-size/2 + $socket-margin
-.tooltip
-  .tooltiptext
-    width: $node-width
-    background-color: black
-    color: #fff
-    padding: 5px 10px
-    border-radius: 6px
-    position: absolute
-    text-align: left
-    z-index: 3
-    .node_description
-      font-size: 14px
-    .socket_type
-      font-weight: bold
-      font-size: 14px
-  .output-tooltip
-    left: 105%
-  .input-tooltip
-    right: 105%
-  .node-tooltip
-    left: 105%
-    top: 5px
 </style>
